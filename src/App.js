1c27c6c1b816c40969ff80fa284ccb98f36d44c3
repenaattori <reimarii@oamk.fi>
2022-11-06 12:20:02 +0200

    Basic timeline chart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import data from './data/companydata.json';
+import {DateTime} from 'luxon';
+import 'chartjs-adapter-luxon';
+import {Chart} from 'chart.js/auto'
+import {Line} from 'react-chartjs-2';
+
+
+function convertToLuxonDate(dataObj){
+  return {...dataObj, time: DateTime.fromISO(dataObj.time)}
+}
 
 function App() {
+
+  const options = {
+    scales:{
+        x:{
+            type: 'time'
+        },
+    },
+    elements:{
+      point:{
+          radius: 0
+      }
+  }
+  };
+
+  console.log(data);
+  const graphData = {
+    datasets: [
+      {
+        label: "Annual revenues",
+        data: data.revenues.map(d => convertToLuxonDate(d)),
+        backgroundColor: ['#00000'],
+        borderColor: '#000000',
+        borderWidth: 2,
+        parsing:{
+          xAxisKey: 'time',
+          yAxisKey: 'revenue'
+        }
+      },
+      {
+        label: "Investments",
+        data: data.investments.map(d => convertToLuxonDate(d)),
+        backgroundColor: ['#e49917'],
+        borderColor: '#e49917',
+        borderWidth: 2,
+        parsing:{
+          xAxisKey: 'time',
+          yAxisKey: 'expenses'
+        }
+      }
+    ],
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap'}}>
+            <div><Line options={options} data={graphData} width={'1000px'} height={'500px'}/></div>
+        </div>
   );
 }
 
