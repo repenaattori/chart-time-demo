0d7cd6c605690433c1ce41d5868ef5c317e7521f
repenaattori <reimarii@oamk.fi>
2022-12-05 16:27:22 +0200

    some little additions

--- src/App.js
+++ src/App.js
@@ -3,24 +3,27 @@ import {DateTime} from 'luxon';
 import 'chartjs-adapter-luxon';
 import {Chart} from 'chart.js/auto'
 import {Line} from 'react-chartjs-2';
+import { useEffect, useState } from 'react';
 
 
 function convertToLuxonDate(dataObj){
-  return {...dataObj, time: DateTime.fromISO(dataObj.time)}
+  return {...dataObj, time: dataObj.time}
 }
 
 function App() {
+
+  //const [data, setData] = useState([]);
+
+  // useEffect(()=>{
+  //   axios.get("")
+  //   then( resp => setData(resp.data) )
+  // },[]);
 
   const options = {
     scales:{
         x:{
             type: 'time'
         },
-    },
-    elements:{
-      point:{
-          radius: 0
-      }
     }
   };
 
@@ -28,18 +31,18 @@ function App() {
     datasets: [
       {
         label: "Annual revenues",
-        data: data.revenues.map(d => convertToLuxonDate(d)),
+        data: data,
         backgroundColor: ['#00000'],
         borderColor: '#000000',
         borderWidth: 2,
         parsing:{
           xAxisKey: 'time',
-          yAxisKey: 'revenue'
+          yAxisKey: 'global'
         }
       },
       {
         label: "Investments",
-        data: data.investments.map(d => convertToLuxonDate(d)),
+        data: data.investments,
         backgroundColor: ['#e49917'],
         borderColor: '#e49917',
         borderWidth: 2,
@@ -51,10 +54,35 @@ function App() {
     ],
   }
 
+
   return (
-     <div style={{ width: '50%'}}>
-            <Line options={options} data={graphData}/>
-        </div>
+      <div>
+          <div style={{ width: '1000px'}}>
+              <Line options={options} data={graphData}/>
+          </div>
+          <input type={'range'} value={0} onChange={x => console.log(x)}></input>
+      </div>
+             
+  );
+}
+
+function MyChart({options,data}){
+
+  const [zoom, setZoom] = useState(1000);
+
+  const zooming = (e)=>{
+    setZoom(x => x+(e.deltaY/5));
+  }
+
+  const style = {
+    postion: 'fixed',
+    width: zoom+'px'
+  }
+  
+  return(
+          <div style={style} onWheel={zooming}>
+              <Line options={options} data={data}/>
+          </div>
   );
 }
 
